ae89cce718d22ff6018959e0a5be6f8b23a99973
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2017-11-17 15:26:00 -0800

    Initial commit

---
+++ Basic Python Notes.py
@@ -0,0 +1,20 @@
+print("Hello world,")
+print("Welcome to Class")
+
+# This is how you add a comment
+# They are really useful!
+# The program ignores these lines
+
+'''This is a bunch text
+that will all be ignored
+because it is in between 3 quotation
+marks
+'''
+#Variables
+a =('Hello World')
+print(a)
+
+b = 5
+c = 7
+print(b + c)
+print(-1 - (-5))
